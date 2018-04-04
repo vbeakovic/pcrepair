91c6b64f6732b09e6dcf5875c10c4a9c69ec5960
vbeakovic <valter.beakovic@gmail.com>
2018-04-04 19:15:49 +0200

    Preparing the starting app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const hbs = require('hbs');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
@@ -12,7 +13,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,4 +25,27 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+module.exports = router;
