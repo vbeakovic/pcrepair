3c9b17b401b514f33797e8729a8c6a22d4d69429
vbeakovic <valter.beakovic@gmail.com>
2018-04-08 20:33:03 +0200

    Mail sending handled

--- routes/contact.js
+++ routes/contact.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 var router = express.Router();
 
-/* GET home page. */
+
 
 router.get('/', function(req, res, next) {
   res.render('contact', {
     title: 'Contact',
     isContact: true,
     currentYear: new Date().getFullYear()
+  });
+});
+
+// send email
+router.post('/send', function(req, res, next) {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: '', // insert email
+      pass: ''  // insert password
+    }
+  });
+  var mailOptions = {
+    from: '"Valter Beaković" <valter.beakovic@gmail.com>',
+    to: 'valter@abeona.hr',
+    subject: 'Hello from PCRepair',
+    text: 'You have a submission from... Name: ' + req.body.name + 'Email: ' + req.body.email + 'Mesagge: ' + req.body.message,
+    html: '<p>You have a submission from...</p><ul><li>Name: ' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Mesagge: ' + req.body.message + '</li></ul>',
+  }
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message Sent: ' + info.response);
+    res.redirect('/');
   });
 });
 
